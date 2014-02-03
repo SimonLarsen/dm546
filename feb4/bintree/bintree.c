c7f6ca1a012c60954c6b8ed2603a7182d4ef4e1f
Simon Larsen <simonhffh@gmail.com>
2014-02-03 17:59:47 +0100

    Added lookup and delete to bintree

--- feb4/bintree/bintree.c
+++ feb4/bintree/bintree.c
@@ -10,24 +10,19 @@ bintree *create_bintree() {
 	return tree;
 }
 
-node *create_node(int key) {
+node *create_node(int key, int value) {
 	node *n = malloc(sizeof(node));
 	n->left = 0;
 	n->right = 0;
 	n->p = 0;
 	n->key = key;
+	n->value = value;
 	return n;
 }
 
-node *insert(bintree *tree, int key) {
-	// Insert as root if tree is empty
-	if(tree->root == 0) {
-		tree->root = create_node(key);
-		return;
-	}
-
+node *insert(bintree *tree, int key, int value) {
 	// Find parent
-	node *x = root;
+	node *x = tree->root;
 	node *p = 0;
 	while(x != 0) {
 		p = x;
@@ -38,15 +33,105 @@ node *insert(bintree *tree, int key) {
 			x = x->right;
 		}
 	}
-
+	// Insert as root
+	node *n = create_node(key, value);
+	n->p = p;
+	if(p == 0) {
+		tree->root = n;
+	}
 	// Insert as left child
-	if(key <= p->key) {
-		p->left = create_node(key);
-		p->left->p = p;
+	else if(key <= p->key) {
+		p->left = n;
 	}
 	// Insert as right child
 	else {
-		p->right = create_node(key);
-		p->right->p = p;
+		p->right = n;
 	}
+	// Return newly created node
+	return n;
+}
+
+node *lookup(bintree *tree, int key) {
+	node *x = tree->root;
+	while(x != 0 && key != x->key) {
+		if(key <= x->key) {
+			x = x->left;
+		} 
+		else {
+			x = x->right;
+		}
+	}
+	return x;
+}
+
+node *minimum(node *x) {
+	while(x->left != 0) {
+		x = x->left;
+	}
+	return x;
+}
+void delete(bintree *tree, node *z) {
+	node *y;
+	if(z->left == 0) {
+		transplant(tree, z, z->right);
+	}
+	else if(z->right == 0) {
+		transplant(tree, z, z->left);
+	}
+	else {
+		y = minimum(z->right);
+		if(y->p != z) {
+			transplant(tree, y, y->right);
+			y->right = z->right;
+			y->right->p = y;
+		}
+		transplant(tree, z, y);
+		y->left = z->left;
+		y->left->p = y;
+	}
+}
+
+void transplant(bintree *tree, node *u, node *v) {
+	if(u->p == 0) {
+		tree->root = v;
+	}
+	else if(u == u->p->left) {
+		u->p->left = v;
+	}
+	else {
+		u->p->right = v;
+	}
+	if(v != 0) {
+		v->p = u->p;
+	}
+}
+
+void print_inorder(node *n) {
+	if(n == 0) return;
+	print_inorder(n->left);
+	printf("(%d,%d)\n", n->key, n->value);
+	print_inorder(n->right);
+}
+
+int main() {
+	bintree *tree = create_bintree();
+
+	// Insert four nodes
+	node *n42 = insert(tree, 42, 123);
+	insert(tree, 16, 42);
+	node *n64 = insert(tree, 64, 999);
+	insert(tree, 4, 31);
+	// Print sorted on keys
+	print_inorder(tree->root);
+
+	// Delete two nodes and print again
+	printf("\nDeleting (42,123) and (64,999)\n");
+	delete(tree, n42);
+	delete(tree, n64);
+	print_inorder(tree->root);
+
+	// Search for node with key 16
+	node *n16 = lookup(tree, 16);
+	printf("\nlookup(tree, 16) = (%d,%d)\n", n16->key, n16->value);
+	return 0;
 }
--- feb4/bintree/bintree.h
+++ feb4/bintree/bintree.h
@@ -1,11 +1,12 @@
 #ifndef __BINTREE_H
 #define __BINTREE_H
 
-typedef struct {
-	node *left;
-	node *right;
-	node *p;
+typedef struct node_t {
+	struct node_t *left;
+	struct node_t *right;
+	struct node_t *p;
 	int key;
+	int value;
 } node;
 
 typedef struct {
@@ -13,9 +14,14 @@ typedef struct {
 } bintree;
 
 bintree *create_bintree();
-node *create_node(int key);
+node *create_node(int key, int value);
 
-void insert(bintree *tree, int key);
-void delete(
+node *insert(bintree *tree, int key, int value);
+node *lookup(bintree *tree, int key);
+node *minimum(node *x);
+void delete(bintree *tree, node *z);
+void transplant(bintree *tree, node *u, node *t);
+
+void print_inorder(node *n);
 
 #endif
