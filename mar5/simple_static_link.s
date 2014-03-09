fad1300d60b384e216c0bea2369127ebf0279aa6
Simon Larsen <simonhffh@gmail.com>
2014-03-09 20:14:54 +0100

    Added simple static link implementation from mar5

---
+++ mar5/simple_static_link.s
@@ -0,0 +1,65 @@
+# Implementation of the following program
+# using static link
+#
+# var x:int;
+#
+# func a() : int
+#     func b() : int
+#         write x;
+#         return 0;
+#     end b
+
+#     return b();
+# end a
+#
+# x = 42;
+# a();
+
+num:
+	.string "%d\n"
+
+.text
+.globl main
+
+a:
+	pushl	%ebp
+	movl	%esp, %ebp
+
+	pushl	%ebp		# Push static link
+	call	b
+	addl	$4, %esp
+
+	movl	%ebp, %esp
+	popl	%ebp
+	ret
+
+b:
+	pushl	%ebp
+	movl	%esp, %ebp
+
+	movl	8(%ebp), %ecx	# Follow static link
+	movl	8(%ecx), %ecx	# Follow again
+	movl	-4(%ecx), %eax	# Retrieve x from stack
+
+	pushl	%eax
+	pushl	$num
+	call	printf			# Print x
+	addl	$8, %esp
+	xorl	%eax, %eax
+
+	movl	%ebp, %esp
+	popl	%ebp
+	ret
+
+main:
+	pushl	%ebp
+	movl	%esp, %ebp
+
+	pushl	$42			# Set x = 42
+	pushl	%ebp		# Push static link
+	call	a
+	addl	$8, %esp
+
+	movl	%ebp, %esp
+	popl	%ebp
+	ret
